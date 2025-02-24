0afbb984e4bc22d2446ec6c832c3849724784592
thuongtran1210 <thuongtran24092001@gmail.com>
2025-02-24 22:28:40 +0700

    add particlesystem for enemy
    Thêm hiệu ứng chết cho Enemy

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.U2D.Aseprite;
@@ -9,13 +10,17 @@ public class EnemyMovement : MonoBehaviour
     private Player player;
     [Header("Settings")]
     [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private float playerDetectionRadius;
+
+    [Header("Effects")]
+    [SerializeField] private ParticleSystem passAwayParticles;
     // Start is called before the first frame update
     void Start()
     {
         player = FindAnyObjectByType<Player>();
         if (player == null)
         {
-            Debug.LogWarning("Khong thay Object nao co ten Player");
+            
             Destroy(gameObject);
         }
     }
@@ -23,8 +28,37 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        FollowPlayer();
+        TryAttack();
+      
+    }
+    private void FollowPlayer()
+    {
         Vector2 direction = (player.transform.position - this.transform.position).normalized;
-        Vector2 targetPostion =(Vector2)this.transform.position + direction * moveSpeed * Time.deltaTime;
+        Vector2 targetPostion = (Vector2)this.transform.position + direction * moveSpeed * Time.deltaTime;
         this.transform.position = targetPostion;
+    }
+    private void TryAttack()
+    {
+        float distanceToPlayer = Vector2.Distance(this.transform.position, player.transform.position);
+        if (distanceToPlayer <= playerDetectionRadius)
+        {
+            PassAway();
+          
+        }
+    }
+
+    private void PassAway()
+    {
+        // Huy lien ket voi object parent
+        passAwayParticles.transform.SetParent(null);
+        passAwayParticles.Play();
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(this.transform.position, playerDetectionRadius);
     }
 }
