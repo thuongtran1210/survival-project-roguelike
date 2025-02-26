cea03dd7ff86aa991c17c96c9e332abc108b6ac9
thuongtran1210 <thuongtran24092001@gmail.com>
2025-02-26 13:41:47 +0700

    Add Player health bar
    Thêm PlayerHealthBar

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,35 +1,54 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class PlayerHealth : MonoBehaviour
 {
     [Header("Settings")]
-    [SerializeField] private int health;
+    [SerializeField] private int maxHealth;
+    private int health;
+    [Header("Elements")]
+    [SerializeField] private Slider healthSilder;
+    [SerializeField] private TextMeshProUGUI healthText;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+       this.health = maxHealth;
+        UpdateUI();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
     public void TakeDamage(int damage)
     {
         int realDamage = Mathf.Min(damage, health);
         this.health -= realDamage;
+
+        UpdateUI();
+
+        Debug.Log("Health: " + this.health);
+
         if (health <= 0)
         {
             PassAway();
         }
     }
 
+
     private void PassAway()
     {
         Debug.Log("Die");
+        SceneManager.LoadScene(0);
+    }
+    private void UpdateUI()
+    {
+        float healthBarValue = (float)this.health / maxHealth;
+        healthSilder.value = healthBarValue;
+        healthText.text = health + " / " + maxHealth;
     }
 }
