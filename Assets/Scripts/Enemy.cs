02548a9a874f30cf81654f65bc5ace8e6e37baa3
thuongtran1210 <thuongtran24092001@gmail.com>
2025-02-26 11:20:24 +0700

    Refactory Enemy
    Tái cấu trúc lại code

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(EnemyMovement))]
+public class Enemy : MonoBehaviour
+{
+    [Header("Components")] 
+    private EnemyMovement movement;
+    [Header("Elements")]
+    private Player player;
+
+    [Header("Spawn Indicator")]
+    [SerializeField] SpriteRenderer renderer;
+    [SerializeField] SpriteRenderer spawnIndicator;
+    private bool hasSpawned = false;
+
+    [Header("Attack")]
+    [SerializeField] private int damage;
+    [SerializeField] private float attackFrequency;
+    [SerializeField] private float playerDetectionRadius;
+    private float attackDelay;
+    private float attackTimer;
+
+    [Header("Effects")]
+    [SerializeField] private ParticleSystem passAwayParticles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = GetComponent<EnemyMovement>();
+        player = FindAnyObjectByType<Player>();
+        if (player == null)
+        {
+
+            Destroy(gameObject);
+        }
+        StartSpawnSequence();
+
+        attackDelay = 1f / attackFrequency;
+        
+    }
+    private void StartSpawnSequence()
+    {
+        SetRendererVisibility(false);
+        Vector3 targetScale = spawnIndicator.transform.localScale * 1.2f;
+        LeanTween.scale(spawnIndicator.gameObject, targetScale, .3f)
+            .setLoopPingPong(4)
+            .setOnComplete(SpawnSequenceCompleted);
+       
+    }
+
+    private void SpawnSequenceCompleted()
+    {
+        SetRendererVisibility(true);
+        hasSpawned = true;
+        movement.StorePlayer(player);
+    }
+    private void SetRendererVisibility(bool visibility = true)
+    {
+        renderer.enabled = visibility;
+        spawnIndicator.enabled = !visibility;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (attackTimer >= attackDelay)
+        {
+            TryAttack();
+        }
+        else
+        {
+            Wait();
+        }
+    }
+    private void Wait()
+    {
+        attackTimer += Time.deltaTime;
+    }
+    private void TryAttack()
+    {
+        float distanceToPlayer = Vector2.Distance(this.transform.position, player.transform.position);
+        if (distanceToPlayer <= playerDetectionRadius)
+        {
+            Attack();
+
+        }
+    }
+    private void Attack()
+    {
+        attackTimer = 0f;
+    }
+
+    private void PassAway()
+    {
+        // Huy lien ket voi object parent
+        passAwayParticles.transform.SetParent(null);
+        passAwayParticles.Play();
+        Destroy(gameObject);
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(this.transform.position, playerDetectionRadius);
+    }
+}
--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -8,94 +8,21 @@ public class EnemyMovement : MonoBehaviour
 {
     [Header("Elements")]
     private Player player;
-
-    [Header("Spawn Indicator")]
-    [SerializeField] SpriteRenderer renderer;
-    [SerializeField] SpriteRenderer spawnIndicator;
-    private bool hasSpawned = false;
-
     [Header("Settings")]
     [SerializeField] private float moveSpeed;
-    [SerializeField] private float playerDetectionRadius;
-
-    [Header("Effects")]
-    [SerializeField] private ParticleSystem passAwayParticles;
-    // Start is called before the first frame update
-    void Start()
-    {
-        player = FindAnyObjectByType<Player>();
-        if (player == null)
-        {
-            
-            Destroy(gameObject);
-        }
-        // An renderer cua Enemy
-        renderer.enabled = false;
-        // Hien chi bao xuat hien
-        
-        spawnIndicator.enabled = true;
-        // Animation (scale) cho chi bao xuat hien
-        Vector3 targetScale = spawnIndicator.transform.localScale * 1.2f;
-        LeanTween.scale(spawnIndicator.gameObject,targetScale, .3f)
-            .setLoopPingPong(4)
-            .setOnComplete(SpawnSequenceCompleted);
-
-        // Hien thi ke thu sau khi thuc hien Anmation Indicator
-        // An chi bao xuat hien
-        // Thuc hien  FollowPlayer Attackk
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (!hasSpawned)
-            return;
-
-        FollowPlayer();
-        TryAttack();
-    
-
-      
+        if (player != null)
+            FollowPlayer(); 
     }
-    //Thuc hien sau khi Animation Pingpong Indicator thuc hien xong
-    private void SpawnSequenceCompleted()
+    public void StorePlayer(Player player)
     {
-        // Hien renderer cua Enemy
-        renderer.enabled = true;
-        // An chi bao xuat hien
-
-        spawnIndicator.enabled = false;
-        hasSpawned = true;
-
-
+        this.player = player;
     }
     private void FollowPlayer()
     {
         Vector2 direction = (player.transform.position - this.transform.position).normalized;
         Vector2 targetPostion = (Vector2)this.transform.position + direction * moveSpeed * Time.deltaTime;
         this.transform.position = targetPostion;
-    }
-    private void TryAttack()
-    {
-        float distanceToPlayer = Vector2.Distance(this.transform.position, player.transform.position);
-        if (distanceToPlayer <= playerDetectionRadius)
-        {
-            PassAway();
-          
-        }
-    }
-
-    private void PassAway()
-    {
-        // Huy lien ket voi object parent
-        passAwayParticles.transform.SetParent(null);
-        passAwayParticles.Play();
-        Destroy(gameObject);
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(this.transform.position, playerDetectionRadius);
     }
 }
